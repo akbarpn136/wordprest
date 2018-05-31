ad735be9e6d5e79560715bb82655d6356a2e5736
akbarpn136 <akbarpn136@gmail.com>
2018-05-31 11:23:33 +0700

    memperbaiki kesalahan penulisan fetch url

--- src/store/halaman/actions.js
+++ src/store/halaman/actions.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const fetchHalaman = (state) => {
-    return axios.get('http://127.0.0.1:8080/wp-json/wp/v2/pages?_embed', {
+    return axios.get('http://127.0.0.1:8080/wp-json/wp/v2/pages/', {
         params: {
             _embed: true
         }
