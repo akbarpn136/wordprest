4216259c8dc365d1ac8ce49142e467dec3ee2bf0
akbarpn136 <akbarpn136@gmail.com>
2018-05-31 11:21:38 +0700

    konfigurasi layout halaman rinci serta tambah fungsi ambil data halaman rinci

---
+++ src/pages/halamanRinci.vue
@@ -0,0 +1,49 @@
+<template>
+  <q-page>
+    <q-card square flat>
+      <q-card-media>
+        <img :src="ambilFeatureImage(halaman._embedded)" 
+        :alt="halaman.title.rendered" v-if="ambilFeatureImage(halaman._embedded)">
+      </q-card-media>
+      <q-card-title>
+        {{halaman.title.rendered}}
+      </q-card-title>
+      <q-card-main>
+        <p v-html='halaman.content.rendered' />
+      </q-card-main>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'halamanRinci',
+  data() {
+    return {
+      id: null,
+      halaman: {}
+    }
+  },
+  created() {
+    this.id = this.$route.params.id
+    this.ambilHalaman(this.id)
+  },
+  methods: {
+    ambilHalaman(id) {
+      const hl = this.$store.getters['halaman/getHalaman']
+
+      this.halaman = hl.filter(el => {
+        return (el.id === parseInt(id))
+      })[0]
+    },
+    ambilFeatureImage(data) {
+      if (data['wp:featuredmedia']) {
+        return data['wp:featuredmedia'][0].source_url
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
